8db8283c75f1aa9fa3e8b8a455a5a45598363737
samuelcpr <samuelcpr464@gmail.com>
2023-07-22 14:35:45 -0300

    Adicionar arquivo app.js com configurações do servidor e definição das rotas da API

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Importar as rotas da API
+const usuarioRoutes = require('./routes/usuarioRoutes');
+
+// Configurar o middleware para o uso de JSON nas requisições
+app.use(express.json());
+
+// Definir as rotas da API
+app.use('/usuarios', usuarioRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});

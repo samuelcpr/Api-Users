7536946f3860236ed1d38f2a063ee656666b4c25
samuelcpr <samuelcpr464@gmail.com>
2023-07-22 22:37:16 -0300

    feat: adicionando rota de login para usuarios

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -8,7 +8,8 @@ router.get('/', UsuarioController.listarUsuarios);
 router.get('/:id', UsuarioController.buscarUsuario);
 router.delete('/:id', UsuarioController.deletarUsuario);
 router.put('/:id', UsuarioController.atualizarUsuario);
+router.patch('/:id', UsuarioController.atualizarUsuario);
 
-
+console.log(UsuarioController)
 
 module.exports = router;

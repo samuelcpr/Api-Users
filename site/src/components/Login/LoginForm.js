7f07757aa692220d133df47763565465282df1a7
samuelcpr <samuelcpr464@gmail.com>
2023-07-24 22:51:25 -0300

    feat: criando tela de login

--- site/src/components/Login/LoginForm.js
+++ site/src/components/Login/LoginForm.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './style.css'
 
-const LoginForm = ({ email, senha, onChange, onSubmit, mensagemErro }) => {
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagemErro, setMensagemErro] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Validação simples dos campos de e-mail e senha
+    if (!email || !senha) {
+      setMensagemErro("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    // Cria o objeto com os dados de login
+    const loginData = {
+      email: email,
+      senha: senha
+    };
+
+    try {
+      // Substitua 'URL_DA_SUA_API' pela rota correta da sua API para o login
+      const response = await axios.post('http://localhost:3333/login', loginData);
+      // Login bem-sucedido, redireciona para a página desejada
+      window.location.href = 'https://github.com/samuelcpr'; // Substitua pela página desejada após o login
+    } catch (error) {
+      // Exibe mensagem de erro no login
+      setMensagemErro('Credenciais inválidas. Tente novamente.');
+    }
+
+    // Limpa o formulário após o envio
+    setEmail('');
+    setSenha('');
+  };
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="email">E-mail:</label>
         <input
@@ -11,7 +47,7 @@ const LoginForm = ({ email, senha, onChange, onSubmit, mensagemErro }) => {
           id="email"
           placeholder="Digite seu e-mail"
           value={email}
-          onChange={(e) => onChange('email', e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className="form-group">
@@ -22,15 +58,18 @@ const LoginForm = ({ email, senha, onChange, onSubmit, mensagemErro }) => {
           id="senha"
           placeholder="Digite sua senha"
           value={senha}
-          onChange={(e) => onChange('senha', e.target.value)}
+          onChange={(e) => setSenha(e.target.value)}
         />
       </div>
-      <button type="submit" className="btn btn-primary btn-block">
+      <button type="submit" className="btn btn-primary btn-block" style={{width: '105%'}}>
         Entrar
       </button>
       {mensagemErro && (
         <div className="mt-3 text-danger text-center">{mensagemErro}</div>
       )}
+      <div id='link-login'>
+        <a href='http://localhost:3000/cadastro?' >Cadastre-se</a>
+      </div>
     </form>
   );
 };

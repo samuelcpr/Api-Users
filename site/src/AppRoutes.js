2a84af8e5bb7d8829e0b5da78fb44142ef925642
samuelcpr <samuelcpr464@gmail.com>
2023-08-23 02:36:21 -0300

    add

--- site/src/AppRoutes.js
+++ site/src/AppRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+// import Header from './components/Header';
+// import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Course from './pages/Course';
 import Contact from './pages/Contact';
@@ -15,24 +15,40 @@ import './App.css';
 const AppRoutes = () => {
   return (
     <Router>
-      <div className="app-container">
-        <Header />
-        <Sidebar />
+      
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/account" element={<Account />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/mydashboard" element={<Dashboard />} /> {/* Nova rota */}
             <Route path="/support" element={<Support />} />
             <Route path="/course" element={<Course />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/cadastro" element={<CadastroPage />} />
           </Routes>
         </main>
-      </div>
+     
     </Router>
   );
 };
 
-export default AppRoutes;
+const AuthRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/cadastro" element={<CadastroPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <AppRoutes />
+      <AuthRoutes />
+    </>
+  );
+};
+
+export default App;

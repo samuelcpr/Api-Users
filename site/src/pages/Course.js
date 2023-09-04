e3f2cccb0bd25d3117582d83a8a110c53bde4be5
samuelcpr <samuelcpr464@gmail.com>
2023-09-04 09:03:44 -0300

    chore: adicionando conteudos testes

--- site/src/pages/Course.js
+++ site/src/pages/Course.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
+import './Accout.css'
+
 
 const Course = () => {
   return (
-    <div>
-      <h2>Curso</h2>
-      <p>Descrição do curso e conteúdo aqui.</p>
+    <div id='container'>
+      <h2 id='title'>Nossos Cursos</h2>
+      <div id='barra-lateral'>
+        <Sidebar />
+      </div>
+      <div id='conteudo'>
+        <p>isso é um teste </p>
+      </div>
     </div>
   );
 };

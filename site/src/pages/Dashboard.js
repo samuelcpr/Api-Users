0063770c70e335d3433ef20d22f0bf7b3e5078e3
samuelcpr <samuelcpr464@gmail.com>
2023-09-04 09:03:49 -0300

    chore: adicionando conteudos testes

--- site/src/pages/Dashboard.js
+++ site/src/pages/Dashboard.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
+import './Accout.css'
+
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <p>Painel de controle do usuário aqui.</p>
+    <div id='container'>
+      <h2 id='title'>Dashboard</h2>
+      <div id='barra-lateral'>
+        <Sidebar />
+      </div>
+      <div id='conteudo'>
+        <p>isso é um teste </p>
+      </div>
     </div>
   );
 };
-
 export default Dashboard;

184e26bc504622390793bf4b34b70e63194859fa
samuelcpr <samuelcpr464@gmail.com>
2023-09-04 09:00:31 -0300

    chore: ajustando componente de rotas

--- site/src/AppRoutes.js
+++ site/src/AppRoutes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Header from './components/Header';
-// import Sidebar from './components/Sidebar';
+
 import Home from './pages/Home';
 import Course from './pages/Course';
 import Contact from './pages/Contact';
@@ -12,42 +11,29 @@ import LoginPage from './pages/LoginPage';
 import CadastroPage from './pages/CadastroPage';
 import './App.css';
 
-const AppRoutes = () => {
-  return (
-    <Router>
-      
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/mydashboard" element={<Dashboard />} /> {/* Nova rota */}
-            <Route path="/support" element={<Support />} />
-            <Route path="/course" element={<Course />} />
-          </Routes>
-        </main>
-     
-    </Router>
-  );
-};
-
-const AuthRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/cadastro" element={<CadastroPage />} />
-      </Routes>
-    </Router>
-  );
-};
-
 const App = () => {
   return (
-    <>
-      <AppRoutes />
-      <AuthRoutes />
-    </>
+    <Router>
+       <div>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/cadastro" element={<CadastroPage />} />
+            </Routes>
+          </div>
+     
+          <main className="main-content">
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/support" element={<Support />} />
+              <Route path="/course" element={<Course />} />
+
+            </Routes>
+          </main>
+       
+    </Router>
   );
 };
 

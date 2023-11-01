11be752a7b4aac8f48a39884da3e71b35179e205
samuelcpr <samuelcpr464@gmail.com>
2023-11-01 14:34:20 -0300

    chore: reajustando menu principal para se adequar as novas seções

--- site/src/components/Sidebar.js
+++ site/src/components/Sidebar.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import MinhaImagem from './image/image1.png'
 import { AiOutlineBook, AiOutlineComment, AiOutlineUser, AiOutlineDashboard, AiOutlineRight } from 'react-icons/ai';
 import './style/sidebar.css';
 
 const Sidebar = () => {
   return (
+    
     <aside>
-      <nav>
-        <Link to="/course">
-          <AiOutlineBook /> Curso
-        </Link>
-        <Link to="/contact">
-          <AiOutlineComment /> Fale com a gente
-        </Link>
-        <Link to="/account">
-          <AiOutlineUser /> Minha Conta
-        </Link>
-        <Link to="/dashboard">
+      <div id='logo'>
+          <img src={MinhaImagem} alt="Descrição da imagem" style={{height:'150px'}} />
+        </div>
+      <nav >
+        <NavLink to="/dashboard">
           <AiOutlineDashboard /> Dashboard
-        </Link>
-        <Link to="/support">
+        </NavLink>
+        <NavLink to="/course">
+          <AiOutlineBook /> Curso
+        </NavLink>
+        <NavLink to="/contact">
+          <AiOutlineComment /> Fale com a gente
+        </NavLink>
+        <NavLink to="/account">
+          <AiOutlineUser /> Minha Conta
+        </NavLink>
+
+        <NavLink to="/support">
           <AiOutlineRight /> Suporte
-        </Link>
+        </NavLink>
       </nav>
     </aside>
   );
